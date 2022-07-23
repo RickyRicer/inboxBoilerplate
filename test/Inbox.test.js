0ba49f0bcc499ce4a8aaf453ece5511291b90658
Rick Ansay <rick.ansay@gmail.com>
2022-07-22 22:53:18 -0600

    Contract tests working as designed

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,14 +8,16 @@ const { interface, bytecode } = require('../compile');
 let accounts;
 let inbox;
 
-
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
   //Use one of those accounts to deploy the contract
-  inbox = new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hello there!'] })
-    .send({ from: accounts[0], gas: '1000000' })
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ 
+      data: bytecode, 
+      arguments: ['Hello there!'] 
+    })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
@@ -25,6 +27,12 @@ describe('Inbox', () => {
 
   it('has a default message', async () => {
     const message = await inbox.methods.message().call();
-    assert.equal(message, 'Hello there!')
+    assert.equal(message, 'Hello there!');
+  });
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
   });
 });
